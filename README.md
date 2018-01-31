0a6f76d72d0b2b3ad829c784e341c6ba030b795b
alberto56 <albert@mediatribe.net>
2018-01-31 14:04:37 -0500

    Better example, and do not use the file with errors in the test

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Lint Javascript with [SASS lint](https://www.npmjs.com/package/sass-lint).
 For example:
 
     docker run -v $(pwd)/example:/app/code dcycle/sass-lint --help
-    docker run -v $(pwd)/example:/app/code dcycle/sass-lint .
+    docker run -v $(pwd)/example:/app/code dcycle/sass-lint -v -q -c /sass-lint-rules.yml /app/code/some-sass-file.scss
+    docker run -v $(pwd)/example:/app/code dcycle/sass-lint -v -q -c /sass-lint-rules.yml /app/code/some-sass-file-with-errors.scss
 
 See [this project on the Docker Hub](https://hub.docker.com/r/dcycle/sass-lint/).
--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ docker pull dcycle/sass-lint
 docker build -t local-dcycle-sass-lint-image .
 
 docker run -v $(pwd)/example:/app/code dcycle/sass-lint --help
-docker run -v $(pwd)/example:/app/code dcycle/sass-lint -v -q -c /sass-lint-rules.yml /app/code/*
+docker run -v $(pwd)/example:/app/code dcycle/sass-lint -v -q -c /sass-lint-rules.yml /app/code/some-sass-file.scss
 docker run -v $(pwd)/example:/app/code local-dcycle-sass-lint-image --help
-docker run -v $(pwd)/example:/app/code local-dcycle-sass-lint-image -v -q -c /sass-lint-rules.yml /app/code/*
+docker run -v $(pwd)/example:/app/code local-dcycle-sass-lint-image -v -q -c /sass-lint-rules.yml /app/code/some-sass-file.scss
