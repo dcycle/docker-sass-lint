aed4bb8d49628929a396401ae372ffd192d45cfb
Albert Albala <albert@mediatribe.net>
2023-05-29 15:19:34 -0400

    Provide script to regularly rebuild (#1)

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
 set -e
 docker pull node
-docker pull dcycle/sass-lint
 docker build -t local-dcycle-sass-lint-image .
 
-docker run -v $(pwd)/example:/app/code dcycle/sass-lint --help
-docker run -v $(pwd)/example:/app/code dcycle/sass-lint -v -q -c /sass-lint-rules.yml /app/code/some-sass-file.scss
 docker run -v $(pwd)/example:/app/code local-dcycle-sass-lint-image --help
 docker run -v $(pwd)/example:/app/code local-dcycle-sass-lint-image -v -q -c /sass-lint-rules.yml /app/code/some-sass-file.scss
